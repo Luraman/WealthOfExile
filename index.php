82e8e0b11ee8fe84aaaeb5a24645f33aff1c5c59
Luraman <ponifi3d@gmail.com>
2020-02-28 15:44:56 +0100

    And so it begins

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Page Title</title>
+<title>The biggest of stonks</title>
 </head>
 <body>
 
-<h1>My First Heading</h1>
-<p>My first paragraph.</p>
+<p>Markus er en doublenibba</p>
 
 </body>
 </html>
