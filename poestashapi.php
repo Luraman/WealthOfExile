79850b23ab2873c213443b7aa5a222dc6a2c99ad
Luraman <ponifi3d@gmail.com>
2020-03-02 21:21:02 +0100

    Testing

--- poestashapi.php
+++ poestashapi.php
@@ -12,18 +12,20 @@ function fetchstashes($changeid) {
 }
 
 $changeid = "";
-for ($x = 0; $x <= 5; $x++) {
+for ($x = 0; $x <= 1; $x++) {
     $result = fetchstashes($changeid);
 
-    if ($result["next_change_id"] == $changeid) {
+    if ($result->next_change_id == $changeid) {
         echo "Nothing Found!";
         break;
     } else {
-        $changeid = $result["next_change_id"];
+        $changeid = $result->next_change_id;
     }
 
-    $currencystashes = array_filter($results["stashes"], function ($stash) {
-        return $stash["stashType"] == "PremiumStash";
+    echo $result->stashes[0] . "<br>";
+
+    $currencystashes = array_filter($results->stashes, function ($stash) {
+        return $stash->stashType == "PremiumStash";
     });
 
     if (!empty($currencystashes)) {
