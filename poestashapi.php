7056d065df3f579683bbce210732e4c62cc24139
Luraman <ponifi3d@gmail.com>
2020-03-02 17:55:30 +0100

    Add code to poestashapi

--- poestashapi.php
+++ poestashapi.php
@@ -1,3 +1,30 @@
 <?php
-echo "Testing!";
+$url = "http://api.pathofexile.com/public-stash-tabs";
+
+function fetchstashes($changeid) {
+    $client = curl_init($GLOBALS["url"] . "?id=:" . $changeid);
+    curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
+
+    $response = curl_exec($client);
+    $result = json_decode($response);
+    return $result;
+}
+
+$changeid = 0;
+$result = fetchstashes($changeid);
+
+if ($result->next_change_id = $changeid) {
+    echo "Nothing Found!";
+    die;
+}
+
+$currencystashes = array_filter($results->stashes, function ($stash) {
+    return $stash["stashType"] == "CurrencyStash";
+});
+
+if (empty($currencystashes)) {
+    echo "Nothing Found!";
+} else {
+    echo $currencystashes[0];
+}
 ?>
